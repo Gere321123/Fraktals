560c29dc61f6e84fbddd1c2a675a2996c6b5c44b
Gere_X <52193900+Gere321123@users.noreply.github.com>
2025-01-21 16:46:23 +0100

    ajde

--- coplex.cpp
+++ coplex.cpp
@@ -7,19 +7,33 @@ int main(int argc, char **argv)
   using Complex = std::complex<float>;
   std::vector<Complex> c;
   int index = 0;
-  for (float i = -1; i < 2; i += 0.001)
+  for (float i = -1; i < 2; i += 0.01)
   {
-    for (float j = -1; j < 2; j += 0.001)
+    for (float j = -1; j < 2; j += 0.01)
     {
       c.push_back(Complex(i, j));
     }
     index++;
   }
-
+  std::vector<bool> display;
+  std::complex<float> z_previus;
   for (const auto &complex : c)
   {
-    std::cout << "(" << complex.real() << ", " << complex.imag() << ")" << std::endl;
+    std::complex<float> z = 0;
+    for (int i = 0; i < 7; i++)
+    {
+      z_previus = z;
+      z = z * z + complex;
+    }
+    float distance = std::abs(z - z_previus);
+    if (distance < 1.0f)
+    {
+      display.push_back(false);
+    }
+    else
+    {
+      display.push_back(true);
+    }
   }
-
   return 0;
 }
